3b7898869a1e4dd742879a80cbc3677b64f2c218
ghelgameshra <ghelgameshra3347@gmail.com>
2022-08-05 19:29:16 +0700

    membuat file konfigurasi tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// /** @type {import('tailwindcss').Config} */ 
+// module.exports = {
+//   content: ["./public_html/**/*.{html,js}"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+module.exports = {
+  content: ["./public_html/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
